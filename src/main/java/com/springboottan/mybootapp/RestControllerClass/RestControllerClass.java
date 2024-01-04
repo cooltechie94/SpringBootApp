3a42bc4fe4c512adab8ac9420127457a6acc9a54
Piyush <piyush.prasad94@gmail.com>
2024-01-04 16:39:32 +0530

    adding properties

--- src/main/java/com/springboottan/mybootapp/RestControllerClass/RestControllerClass.java
+++ src/main/java/com/springboottan/mybootapp/RestControllerClass/RestControllerClass.java
@@ -1,10 +1,17 @@
 package com.springboottan.mybootapp.RestControllerClass;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestControllerClass {
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @Value("${coach.name}")
+    private String coachName;
 
     @GetMapping("/")
     public String getHello(){
@@ -13,5 +20,11 @@ public class RestControllerClass {
     @GetMapping("/users")
     public String getUsers(){
         return "Loading...";
+    }
+    @GetMapping("/teaminfo")
+    public String displayTeam(){
+        return "We are "+ teamName + " Football Club. Founded in 1892.  \n" +
+                "The club has won 19 league titles, eight FA Cups & a record nine League Cups." +
+                "Current Coach Name: "+ coachName ;
     }
 }
